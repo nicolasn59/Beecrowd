f54d59b650a18047d62e0f3e5ca2266b076d725c
Nicolas Nogueira <nicolasnogueira59@gmail.com>
2024-10-28 20:33:32 -0300

    Novas Soluções AD-HOC em Python

---
+++ Python/AD-HOC/1387 Og.py
@@ -0,0 +1,6 @@
+# SUBPROGRAMA
+# PROGRAMA PRINCIPAL
+filho, filha = map(int, input().split())
+while filho != 0 or filha != 0:
+    print(filho + filha)
+    filho, filha = map(int, input().split())
